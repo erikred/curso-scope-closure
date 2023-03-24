1125d19cceb1bd9b8a03ca59eaeaa790afd441f1
Pariksit Ortiz <pariksit.ortiz@gmail.com>
2023-03-24 11:43:44 -0500

    First commit - class 2 - global function

---
+++ source/scope/global.js
@@ -0,0 +1,31 @@
+// Variables
+
+var a; // declarando
+
+var b = 'b'; // declaramos / asignamos
+
+b = 'bb'; //reasignacion
+
+var a = 'aa'; //redeclaracion
+
+
+// Global Scope
+
+var fruit = 'Aple'; //global
+
+console.log(fruit);
+
+function bestFruit() {
+    console.log(fruit);
+}
+
+bestFruit();
+
+
+function countries() {
+    country = 'Colombia'; // global
+    console.log(country);
+}
+
+countries();
+console.log(country);
