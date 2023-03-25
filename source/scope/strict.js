8a549a745f08d2673bbe94d4e15ade519b070355
Pariksit Ortiz <pariksit.ortiz@gmail.com>
2023-03-24 20:17:29 -0500

    sixth commit - class 6 - estrict mode

---
+++ source/scope/strict.js
@@ -0,0 +1,8 @@
+'use strict'; // Modo esctricto exije que primero se declare una variable nates de asignarle un valor
+pi = 3.1416;
+console.log(pi);
+
+function myFunction(){
+    'use strict';
+    return pi = 3.1416;
+}
