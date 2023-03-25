ec37ab4fd44bdf507f1176f1cbf3a09a619a48a7
Pariksit Ortiz <pariksit.ortiz@gmail.com>
2023-03-25 13:51:20 -0500

    11th commit - class 11 and 12 - hoisting and debugging

---
+++ source/debug/index.js
@@ -0,0 +1,9 @@
+var a = 'Hello';
+
+function hello() {
+    let b = 'Hello World';
+    const c = 'Hello World!';
+    debugger; // hace una pausa en el navegador
+}
+
+hello(); // se copia y se ejecuta en el navegador en consola con su herramienta de debugging
---
+++ source/hoisting/index.js
@@ -0,0 +1,12 @@
+console.log(nameofDog); // se muestra undefined
+var nameofDog = 'Elmo';
+console.log(nameofDog); // aca si tiene valor
+
+
+nameOfDogFunction();
+
+function nameOfDogFunction() {
+    console.log(`El mejor perrito es ${elmo}`);// elmo es undefined
+}
+
+var elmo = 'Elmito';
